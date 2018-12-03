4cd270f497d773d09d469420f9c8ef58e51cb1f0
Lee Beenen <l.beenen@diract.nl>
2018-12-03 17:23:55 +0100

    copy over project structure

---
+++ Days/Days.cs
@@ -0,0 +1,20 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+public static partial class Days
+{
+  private static string OutputResult(string part1, string part2)
+  {
+    return $"{Environment.NewLine}- Part 1: {part1}{Environment.NewLine}- Part 2: {part2}";
+  }
+
+  public static string Day1()
+  {
+
+  }
+}
