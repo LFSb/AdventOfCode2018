f5e9159666974f2d6cc66d801fb899a4560b6737
Lee Beenen <l.beenen@diract.nl>
2018-12-10 22:01:04 +0100

    Day 2, p2

--- Days/Days.cs
+++ Days/Days.cs
@@ -21,28 +21,30 @@ public static partial class Days
 
     var freqDict = new Dictionary<int, bool>();
 
-    while(!freqDict.Any(x => x.Value))
+    while (!freqDict.Any(x => x.Value))
     {
-      for(var row = 0; row < changes.Length; row++)
+      for (var row = 0; row < changes.Length; row++)
       {
         var change = changes[row];
 
         var op = change[0];
         var number = int.Parse(change.Substring(1));
 
-        switch(op)
+        switch (op)
         {
           case '+':
-          {
-            current += number;
-          } break;
+            {
+              current += number;
+            }
+            break;
           case '-':
-          {
-            current -= number;
-          } break;
+            {
+              current -= number;
+            }
+            break;
         }
 
-        if(freqDict.ContainsKey(current))
+        if (freqDict.ContainsKey(current))
         {
           freqDict[current] = true;
           break;
@@ -64,47 +66,63 @@ public static partial class Days
     var twoCount = 0;
     var threeCount = 0;
 
-    foreach(var str in input)
+    foreach (var str in input)
     {
       var grouped = str.GroupBy(x => x);
-      
-      if(grouped.Any(x => x.Count() == 2))
+
+      if (grouped.Any(x => x.Count() == 2))
         twoCount++;
-      if(grouped.Any(x => x.Count() == 3))
+      if (grouped.Any(x => x.Count() == 3))
         threeCount++;
     }
-    
-    //Not quite right. Comparing wrong?
-    var ordered = string.Join("", input.OrderBy(x => x, new HammingComparer()).Take(2).GroupBy(x => x).Where(z => z.Count() > 1));
 
-    return OutputResult((twoCount * threeCount).ToString(), ordered);
+    var testInput = new[] { "abcde", "fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz" };
+    var ham = new CharDifferenceComparer();
+
+    var score = input.ToDictionary(x => x, 
+    x => input.Select(y => new {
+      Value = y,
+      Score = ham.Compare(x, y)
+    }))
+    .First(x => x.Value.Any(z => z.Score == 1));
+    
+    var val = score.Value.First(x => x.Score == 1).Value;
+
+    var interSectingValue = string.Empty;
+
+    for(var i = 0; i < score.Key.Length; i++)
+    {
+      if(score.Key[i] == val[i])
+      {
+        interSectingValue += score.Key[i];
+      }
+    }
+
+    return OutputResult((twoCount * threeCount).ToString(), interSectingValue);
   }
 
-  public class HammingComparer : IComparer<string>
+  public class CharDifferenceComparer : IComparer<string>
   {
     public int Compare(string x, string y)
     {
-      return CalculateHammingDistance(Encoding.UTF8.GetBytes(x), Encoding.UTF8.GetBytes(y));
+      return CalculateDifference(Encoding.UTF8.GetBytes(x), Encoding.UTF8.GetBytes(y));
     }
   }
 
-  public static Int32 CalculateHammingDistance(byte[] input1, byte[] input2)
+  public static Int32 CalculateDifference(byte[] input1, byte[] input2)
   {
     var score = 0;
 
-    if(input1.Length != input2.Length)
+    if (input1.Length != input2.Length)
     {
       throw new InvalidDataException("Input lengths are not equal. Aborting...");
     }
 
-    for(var idx = 0; idx < input1.Length; idx++)
+    for (var idx = 0; idx < input1.Length; idx++)
     {
-      var val = input1[idx] ^ input2[idx];
-
-      while(val != 0)
+      if(input1[idx] != input2[idx])
       {
         score++;
-        val &= val - 1;
       }
     }
 
